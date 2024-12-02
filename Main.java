563d374e2ba15c448d3a673549033283890350aa
lordjav <lord_javius@hotmail.com>
2024-12-01 21:54:40 -0500

    Add Angular packages

--- Main.java
+++ Main.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter name ");
+        String name = scanner.nextLine();
+        System.out.println("Hi, " + name);
+        scanner.close();
     }
 }
